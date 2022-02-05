d20f292bd1ce97f9f2b729203b0cd719ae3db3eb
SFENCE <sfence.software@gmail.com>
2022-02-05 21:44:20 +0100

    Brick recipes in Hades style.

--- crafting.lua
+++ crafting.lua
@@ -2,8 +2,8 @@
 minetest.register_craft({
 	output = "hades_default:sandstonebrick 4",
 	recipe = {
-		{"hades_default:sandstone", "hades_default:sandstone"},
-		{"hades_default:sandstone", "hades_default:sandstone"},
+		{"hades_default:sandstone", "hades_default:sandstone", ""},
+		{"", "hades_default:sandstone", "hades_default:sandstone"},
 	}
 })
 
@@ -19,8 +19,8 @@ minetest.register_craft({
 minetest.register_craft({
 	output = "hades_default:desert_sandstone_brick 4",
 	recipe = {
-		{"hades_default:desert_sandstone", "hades_default:desert_sandstone"},
-		{"hades_default:desert_sandstone", "hades_default:desert_sandstone"},
+		{"hades_default:desert_sandstone", "hades_default:desert_sandstone", ""},
+		{"", "hades_default:desert_sandstone", "hades_default:desert_sandstone"},
 	}
 })
 
@@ -36,8 +36,8 @@ minetest.register_craft({
 minetest.register_craft({
 	output = "hades_default:silver_sandstone_brick 4",
 	recipe = {
-		{"hades_default:silver_sandstone", "hades_default:silver_sandstone"},
-		{"hades_default:silver_sandstone", "hades_default:silver_sandstone"},
+		{"hades_default:silver_sandstone", "hades_default:silver_sandstone", ""},
+		{"", "hades_default:silver_sandstone", "hades_default:silver_sandstone"},
 	}
 })
 
